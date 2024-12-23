8c645b3af17329e172a0513c6a38796fb5de5da5
Kirananagod <kirananagod24@gmail.com>
2024-12-23 15:42:32 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add smooth scrolling to navigation
+document.querySelectorAll('.navbar a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
